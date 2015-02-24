d2ffb8a9c65d7cb603317bc5f474053f692f103a
Matteo Scandolo <matteo.scandolo@gmail.com>
2015-02-24 23:51:43 +0100

    Basic tests, needs explanation

---
+++ spec/ninjaHelper.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var mongoose = require('mongoose');
+require('../models/ninja');
+var Ninja = mongoose.model('Ninja');
+var P = require('bluebird');
+
+var testNinja = {
+	name: 'testNinja',
+	age: 42
+};
+
+exports.ninja = testNinja;
+
+exports.nuke = P.promisify(function(done){
+	Ninja.remove({},done);
+});
+
+exports.insert = P.promisify(function(done){
+	var ninja = new Ninja(testNinja);
+	ninja.save(done);
+});
